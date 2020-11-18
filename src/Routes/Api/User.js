9fbcebf9f7fe42950d31bfbda3e67fa02f4b9bcb
Percy Musaka <pmusaka@instantia.co.uk>
2020-11-18 19:21:16 +0000

    fixed some annoying none sense

--- src/Routes/Api/User.js
+++ src/Routes/Api/User.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { UserController } = require('../../Controller')
+const { UserController } = require('../../Controller/Index')
 const { Register } = UserController
 
 router.post('/register', Register)
